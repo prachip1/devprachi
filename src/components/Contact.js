02c4de2b992a22d4eae7d92dbc1896c7c11240ec
Prachi P <prachip1908@gmail.com>
2022-01-07 20:52:35 +0530

    commit now

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,12 +2,7 @@ import React from "react";
 
 export default function Contact() {
   
-
- 
-
-
-
-  return (
+return (
     <section id="contact" className="relative bg-black-light">
       <div className="container w-full px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
            <div className="w-full flex flex-col text-center ">
